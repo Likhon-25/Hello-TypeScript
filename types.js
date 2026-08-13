2db4204798f0b6b28c175afa1ade3970b9c8094b
Likhon-25 <likhonmondol327@gmail.com>
2026-08-13 23:30:29 +0600

    learn primitive Types in typescript

---
+++ types.js
@@ -0,0 +1,9 @@
+"use strict";
+let districe = "Rangpur";
+districe = "Gaibandha";
+// districe = 440
+const stock = 100;
+const isAvailable = true;
+let input = 'hello';
+input = 450;
+console.log(districe);
---
+++ types.ts
@@ -0,0 +1,10 @@
+let districe:string = "Rangpur";
+districe = "Gaibandha"
+// districe = 440
+
+const stock : number = 100;
+const isAvailable : boolean = true;
+
+let input : any = 'hello'
+input = 450;
+console.log(districe);
