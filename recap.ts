8d4e3cadec132a7bb252c0303f153fad3d16c013
Likhon-25 <likhonmondol327@gmail.com>
2026-08-14 00:34:03 +0600

    Recap Object types, literals and optional properties

---
+++ recap.ts
@@ -0,0 +1,17 @@
+const books: string[] = ["book1", "book2", "book3"];
+
+const runs: number[] = [4, 2, 6, 6, 6, 4];
+
+const book: {
+  name: string;
+  author: string;
+  price: number;
+  pages: number;
+  isUsed?: boolean;
+} = {
+  name: "Python Programming",
+  author: "Guido Van Rosum",
+  price: 125,
+  pages: 150,
+//   isUsed: true,
+};
