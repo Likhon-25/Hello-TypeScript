e3dd8a66e1701949c3fdfdd5501b0408d84a88fc
Likhon-25 <likhonmondol327@gmail.com>
2026-08-24 17:40:51 +0600

    problem 4 solved

---
+++ TS Problem Solving M24/src/calculateCartTotal.ts
@@ -0,0 +1,17 @@
+type Product = {
+    name: string,
+    price: number
+}
+const calculateCartTotal = (products : Product[]): number =>{
+    const total : number = products.reduce((acc, item) => acc + item.price, 0)
+
+    return total
+}
+
+const products = [
+    { name: "Keyboard", price: 1500 },
+    { name: "Mouse", price: 800 },
+    { name: "USB Cable", price: 300 }
+];
+ 
+console.log(calculateCartTotal(products));
